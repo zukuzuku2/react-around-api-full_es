ce54029bc49dbdc771739951baff70ba0d67b9d8
Jose Santos <josejaviersantossantana@gmail.com>
2023-05-21 04:10:38 -0400

    Modificar Ruta apiAuth frontEnd

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "api.jose.desarrollointerno.com";
 
 export const signup = ({ email, password }) => {
   return fetch(`${BASE_URL}/signup`, {
